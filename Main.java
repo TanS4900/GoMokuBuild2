5d0bc9c6ea1cf5f26c8104ce7107e1ce8d6a8b48
Tanish Shenoy <58651285+TanS4900@users.noreply.github.com>
2023-05-30 11:46:01 -0700

    Added some more comments to Main.java

--- Main.java
+++ Main.java
@@ -14,24 +14,86 @@ GoMoKu game = new GoMoKu();
 //creates a scanner object (Tanish)
 
 Scanner scanner = new Scanner(System.in);
-
+//Asks the user if they want to play with a computer/AI or Human player (Tanish)
 System.out.println("Do you want to play with the computer(C) or a human player(H)");
 String compplayer=scanner.next();
 if(compplayer.equals("C"))
 {
+	//Asks the user if they selected Computer(C) it will give 3 options to select from (Tanish)
 	System.out.println("Random Placement(R), Ordered Placement(O) or Strategic Placement(S)?");
+	//Random placement has the computer randomly place it, ordered has the computer place it in straight lines, and Strategic has it place it so it almost blocks the player's next move (Tanish)
 	String placement = scanner.next();
 	currentPlayer='X';
-	if(placement.equals("R"))
+	if(placement.equals("R")) //this if else statement is for random placement (Tanish)
 	{
-		while(true)
+		while(true) //This is a while loop that prints out the board (Tanish)
 		{
-			System.out.println("Current board:");
+			System.out.println("Current board:"); //Prints out the board (Tanish)
 
 			game.print();
 			if(currentPlayer=='0')
 			{
-				game.randomPlacement();
+				game.randomPlacement(); //This gives the Computer/AI command to place it randomly on the board
+				currentPlayer='X'; //This gives the Non-AI player their chance
+			}
+			else
+			{
+				System.out.println(currentPlayer
+
+				+ ", enter the (X,Y) coordinates to place your stone and make sure it's seperated by a space (EX: 1 1). Now good luck you do it:");
+
+				//accept the x coordinate (Ryan)
+
+				int x = scanner.nextInt() - 1;
+
+				//accept the y coordinate (Tanish)
+
+				int y = scanner.nextInt() - 1;
+
+				// Place the stone and check for a win (Ryan and Tanish)
+
+				if (game.placeStone(y, x, currentPlayer)) { //nested if statement checking win
+
+					if (game.winner(currentPlayer)) {
+
+						System.out.println(currentPlayer + " wins!");
+
+						//checks who won (Tanish)
+
+						break;
+
+					} 
+					else 
+					{
+
+						//switches the player (Ryan and Tanish)
+						currentPlayer='0';
+					}
+
+
+				} 
+				else 
+				{
+
+					//prints the message if the player entered a pre-occupied spot! (Ryan and Tanish)
+
+					System.out.println("This place has already been occupied. Please select a different spot!");
+
+				}
+
+		}
+}
+	}
+	else if(placement.equals("O")) //This is and else if statement that gives ordered placement for AI/Computer (Tanish)
+	{
+		while(true)
+		{
+			System.out.println("Current board:"); //This prints out the current board (Tanish)
+
+			game.print();
+			if(currentPlayer=='0') //This declares it so computer can start playing (Tanish)
+			{
+				game.orderedPlacement(); //This gives the computer a command to do ordered placement (Tanish)
 				currentPlayer='X';
 			}
 			else
@@ -82,76 +144,16 @@ if(compplayer.equals("C"))
 		}
 }
 	}
-	else if(placement.equals("O"))
+	else if(placement.equals("S")) //This is the else if for Strategic placement for AI (Tanish)
 	{
 		while(true)
 		{
-			System.out.println("Current board:");
+			System.out.println("Current board:"); //Prints out the board (Tanish)
 
 			game.print();
 			if(currentPlayer=='0')
 			{
-				game.orderedPlacement();
-				currentPlayer='X';
-			}
-			else
-			{
-				System.out.println(currentPlayer
-
-				+ ", enter the (X,Y) coordinates to place your stone and make sure it's seperated by a space (EX: 1 1). Now good luck you do it:");
-
-				//accept the x coordinate (Ryan)
-
-				int x = scanner.nextInt() - 1;
-
-				//accept the y coordinate (Tanish)
-
-				int y = scanner.nextInt() - 1;
-
-				// Place the stone and check for a win (Ryan and Tanish)
-
-				if (game.placeStone(y, x, currentPlayer)) {
-
-					if (game.winner(currentPlayer)) {
-
-						System.out.println(currentPlayer + " wins!");
-
-						//checks who won (Tanish)
-
-						break;
-
-					} 
-					else 
-					{
-
-						//switches the player (Ryan and Tanish)
-						currentPlayer='0';
-					}
-
-
-				} 
-				else 
-				{
-
-					//prints the message if the player entered a pre-occupied spot! (Ryan and Tanish)
-
-					System.out.println("This place has already been occupied. Please select a different spot!");
-
-				}
-
-		}
-}
-	}
-	else if(placement.equals("S"))
-	{
-		while(true)
-		{
-			System.out.println("Current board:");
-
-			game.print();
-			if(currentPlayer=='0')
-			{
-				game.strategicPlacement();
+				game.strategicPlacement(); //Gives the computer commands to play strategically (Tanish)
 				currentPlayer='X';
 			}
 			else
